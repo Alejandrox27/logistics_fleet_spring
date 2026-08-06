42ee101823c913b871a6ed6ede36402611782979
Alejandrox27 <didieralejandromejia82@gmail.com>
2026-08-06 14:10:28 -0500

    First commit

---
+++ src/main/java/org/didiermej/logistic_fleet/LogisticFleetApplication.java
@@ -0,0 +1,13 @@
+package org.didiermej.logistic_fleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticFleetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticFleetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/didiermej/logistic_fleet/LogisticFleetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.didiermej.logistic_fleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticFleetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
