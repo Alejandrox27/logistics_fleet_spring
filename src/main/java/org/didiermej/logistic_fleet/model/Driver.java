19812b499f7edb239420b9a47fe87ac4f36a07ef
Alejandrox27 <didieralejandromejia82@gmail.com>
2026-08-07 13:43:52 -0500

    Create model Driver.java from database

---
+++ src/main/java/org/didiermej/logistic_fleet/model/Driver.java
@@ -0,0 +1,39 @@
+package org.didiermej.logistic_fleet.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "drivers")
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_driver")
+    private Integer idDriver;
+
+    @Column(name = "num_identification", nullable = false)
+    private Integer numIdentification;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "lastname", nullable = false, length = 50)
+    private String lastname;
+
+    @Column(name = "second_lastname", length = 50)
+    private String secondLastname;
+
+    @Column(name = "contratation_date")
+    private LocalDate contratationDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", columnDefinition = "driver_status")
+    private DriverStatus status;
+}
