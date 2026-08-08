5a33d226dd5cab887dc19a1735a4a10ff38620a5
Alejandrox27 <didieralejandromejia82@gmail.com>
2026-08-08 16:08:00 -0500

    Create driverServiceImpl functions and a transactional function

--- src/main/java/org/didiermej/logistic_fleet/service/impl/DriverServiceImpl.java
+++ src/main/java/org/didiermej/logistic_fleet/service/impl/DriverServiceImpl.java
@@ -1,37 +1,52 @@
 package org.didiermej.logistic_fleet.service.impl;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.didiermej.logistic_fleet.model.Driver;
 import org.didiermej.logistic_fleet.model.DriverLicense;
+import org.didiermej.logistic_fleet.model.LicenseCategory;
+import org.didiermej.logistic_fleet.repository.DriverLicenseRepo;
+import org.didiermej.logistic_fleet.repository.DriverRepo;
+import org.didiermej.logistic_fleet.repository.LicenseCategoryRepo;
 import org.didiermej.logistic_fleet.service.DriverService;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
+
+@Service
+@RequiredArgsConstructor
 public class DriverServiceImpl implements DriverService {
+    private final DriverRepo driverRepo;
+    private final DriverLicenseRepo driverLicenseRepo;
+    private final LicenseCategoryRepo licenseCategoryRepo;
+
     @Override
     public Driver save(Driver driver) {
-        return null;
+        return driverRepo.save(driver);
     }
 
     @Override
     public Driver update(Integer id, Driver driver) {
-        return null;
+        driver.setIdDriver(id);
+        return driverRepo.save(driver);
     }
 
     @Override
     public List<Driver> findAll() {
-        return List.of();
+        return driverRepo.findAll();
     }
 
     @Override
     public Driver findById(Integer id) {
-        return null;
+        return driverRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("driver with id " + id + " not found"));
     }
 
     @Override
     public void delete(Integer id) {
-
+        driverRepo.deleteById(id);
     }
 
     @Override
@@ -39,6 +54,22 @@ public class DriverServiceImpl implements DriverService {
     public DriverLicense addLicenseToDriver(Integer driverId, Integer categoryId,
                                             LocalDate issueDate, LocalDate expiryDate, String description)
     {
-        return null;
+        // 1. validar que el conductor exista
+        Driver driver = driverRepo.findById(driverId)
+                .orElseThrow(() -> new RuntimeException("The driver with id " + driverId + " does not exist"));
+
+        // 2. validar que lla categoria exista
+        LicenseCategory category = licenseCategoryRepo.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("The category with id " + categoryId + " does not exist"));
+
+
+        DriverLicense driverLicense = new DriverLicense();
+        driverLicense.setDriver(driver);
+        driverLicense.setCategory(category);
+        driverLicense.setDescription(description);
+        driverLicense.setIssueDate(issueDate);
+        driverLicense.setExpiryDate(expiryDate);
+
+        return driverLicenseRepo.save(driverLicense);
     }
 }
